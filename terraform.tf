9bb7135ca914a9f31bf9250c74b571f45c73b4ec
Stefan Magnus Landrø <stefan.landro@gmail.com>
2017-05-12 16:04:47 +0200

    Syntax, variables and count

--- terraform.tf
+++ terraform.tf
@@ -34,15 +34,42 @@ resource "cloudstack_security_group_rule" "web" {
   }
 }
 
+variable "zone" {
+  description = "Cloudstack Availability Zone"
+  type        = "string"
+
+  #default = "ch-dk-2"
+  default = "ch-gva-2"
+}
+
+variable "nb_web_servers" {
+  type        = "string"
+  description = "Number of web servers"
+  default     = "2"
+}
+
 resource "cloudstack_instance" "web" {
+  display_name     = "web${count.index}"
+  count            = "${var.nb_web_servers}"
+  name             = ""
   template         = "Linux CentOS 7.2 64-bit"
   service_offering = "Micro"
-  zone             = "ch-gva-2"
+  zone             = "${var.zone}"
   root_disk_size   = "50"
   expunge          = true
   keypair          = "${cloudstack_ssh_keypair.default.id}"
 
+  affinity_group_ids = [
+    "${cloudstack_affinity_group.web.id}",
+  ]
+
   security_group_ids = [
     "${cloudstack_security_group.web.id}",
   ]
+}
+
+# Make sure web servers don't run on same host
+resource "cloudstack_affinity_group" "web" {
+  name = "web-affinity-group"
+  type = "host anti-affinity"
 }
