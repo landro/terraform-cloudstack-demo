53bb383176157c6d784086613f2f8fb11c686dca
Stefan Magnus Landrø <stefan.landro@gmail.com>
2017-05-12 15:59:09 +0200

    Adding single web server

--- terraform.tf
+++ terraform.tf
@@ -8,3 +8,41 @@ resource "cloudstack_ssh_keypair" "default" {
   name       = "SSH key"
   public_key = "${file("cloudstack_id_rsa.pub")}"
 }
+
+resource "cloudstack_security_group" "web" {
+  name        = "web"
+  description = "Web Servers"
+}
+
+resource "cloudstack_security_group_rule" "web" {
+  security_group_id = "${cloudstack_security_group.web.id}"
+
+  # Allow HTTP, HTTPS and SSH traffic from entire Internet
+  rule {
+    cidr_list = [
+      "0.0.0.0/0",
+    ]
+
+    ports = [
+      80,
+      443,
+      22,
+    ]
+
+    protocol     = "tcp"
+    traffic_type = "ingress"
+  }
+}
+
+resource "cloudstack_instance" "web" {
+  template         = "Linux CentOS 7.2 64-bit"
+  service_offering = "Micro"
+  zone             = "ch-gva-2"
+  root_disk_size   = "50"
+  expunge          = true
+  keypair          = "${cloudstack_ssh_keypair.default.id}"
+
+  security_group_ids = [
+    "${cloudstack_security_group.web.id}",
+  ]
+}
